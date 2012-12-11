fb63121cab27b7a9af04f0a243ffd1b77bdcbe87
Lewis <lbrant1@umbc.edu>
2012-12-10 20:30:01 -0500

    small update
    changed location for potential hash issues

--- Game/src/RPS_soldiers/allClasses.rb
+++ Game/src/RPS_soldiers/allClasses.rb
@@ -67,6 +67,17 @@ class Location
   def getY()
     return posY
   end
+  def eql?(other)
+    if ((other.getX == self.posX) && (other.getY == self.posY)
+      return true
+    else 
+      return false
+    end
+  end
+  def hash
+    return posX * 100 + posY  ##this is ugly (but there should be a better way to get uniq value from a location obv... to work with hash...)
+  end
+end
 
 class Regiment 
   def initialize(Type, soldNum, Location)
